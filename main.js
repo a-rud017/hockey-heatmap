bd58221d5fecac9b4c3d83835d070809d7789e12
Andrew Rudnick <andrewrudnickmi@gmail.com>
2025-10-05 07:55:41 -0400

    change grid size to match rink size and adjust cell function

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const gridContainer = document.querySelector(".grid-container");
 const gridContainerWidth = gridContainer.clientWidth;
+const gridContainerHeight = gridContainer.clientHeight;
 
 function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
         cell.style.width = `${gridContainerWidth / size}px`;
-        cell.style.height = `${gridContainerWidth / size}px`;
+        cell.style.height = `${gridContainerHeight / size}px`;
 
         gridContainer.appendChild(cell);
     }
 }
 
-createGrid(16)
+createGrid(8)
