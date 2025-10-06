3ac01539468d4489c8d50e27380fcb6aab004635
Andrew Rudnick <andrewrudnickmi@gmail.com>
2025-10-06 08:01:51 -0400

    add comments for js functions

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+//Initialize grid variable, grab width and height
 const gridContainer = document.querySelector(".grid-container");
 const gridContainerWidth = gridContainer.clientWidth;
 const gridContainerHeight = gridContainer.clientHeight;
 
+//Create the grid cells and add event listeners to each cell for shading function
 function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const cell = document.createElement("div");
@@ -18,6 +20,7 @@ function createGrid(size) {
 
 createGrid(24)
 
+//Grab the cells current click count, increase the count and save count, increase shade by amount of clicks
 function shadeCell(event) {
     let clicks = Number(event.target.dataset.clicks);
     clicks++;
@@ -26,9 +29,11 @@ function shadeCell(event) {
     event.target.style.backgroundColor = `rgba(255, 0, 0, ${clicks * 0.1})`;
 }
 
+//Initialize reset button variable and add event listener to reset the grid cells
 const resetButton = document.querySelector(".reset-btn");
 resetButton.addEventListener('click', resetGrid)
 
+//Reset the grid cells back to zero clicks and no background color
 function resetGrid() {
     const cells = document.querySelectorAll(".cell")
 
